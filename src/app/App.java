88f599d70407be9126e9009cb8f3a8d9c9b904e9
wegga <wegga@DESKTOP-87CIQ4O>
2017-08-02 05:36:22 -0300

    dfdf

---
+++ src/app/App.java
@@ -0,0 +1,29 @@
+package app;
+
+import java.util.ArrayList;
+
+import Controle.BancoDados;
+import Model.Cliente;
+import Model.Produtos;
+import View.TelaErro;
+
+public class App {
+	
+	public static BancoDados banco = new BancoDados();
+	public static TelaErro erro =  new TelaErro();
+
+	public static void main(String[] args) {
+		
+		App.banco.conectar();
+		 if(App.banco.estaConectado()){
+			 System.out.println("sim");
+		 }
+		 else{
+			 System.out.println("nao");
+		 }
+		 
+		 
+
+	}
+
+}
